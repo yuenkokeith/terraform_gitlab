68a8c173b90d793a6aa801215b2269cf453fb2d3
yuenkokeith <yuenkokeith@yahoo.com.hk>
2023-01-16 12:11:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 module "ec2" {
   source                 = "./ec2"
   public_subnets         = module.vpc.public_subnets
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   key_name               = "MyKey"
   user_data              = filebase64("./userdata.tpl")
   vpc_id                 = module.vpc.vpc_id
